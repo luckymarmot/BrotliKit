69c71ba4b33cbbcdb29106f9e377df957b1561d5
Micha Mazaheri <micha@mazaheri.me>
2018-04-06 00:33:32 +0200

    Fix BrotliKit.h

--- BrotliKit/BrotliKit.h
+++ BrotliKit/BrotliKit.h
@@ -6,16 +6,6 @@
 //  Copyright © 2018 Paw. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
-
-//! Project version number for BrotliKit.
-FOUNDATION_EXPORT double BrotliKitVersionNumber;
-
-//! Project version string for BrotliKit.
-FOUNDATION_EXPORT const unsigned char BrotliKitVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <BrotliKit/PublicHeader.h>
-
 #import "LMBrotliCompression.h"
 #import "LMBrotliCompressor.h"
 #import "NSData+BrotliCompression.h"
