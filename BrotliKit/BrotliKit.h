f47d65ebb6793ba071d5027dec993aba959b5ea8
Micha Mazaheri <micha@mazaheri.me>
2018-04-05 16:52:39 +0200

    Initial Commit

---
+++ BrotliKit/BrotliKit.h
@@ -0,0 +1,19 @@
+//
+//  BrotliKit.h
+//  BrotliKit
+//
+//  Created by Micha Mazaheri on 4/5/18.
+//  Copyright © 2018 Paw. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BrotliKit.
+FOUNDATION_EXPORT double BrotliKitVersionNumber;
+
+//! Project version string for BrotliKit.
+FOUNDATION_EXPORT const unsigned char BrotliKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BrotliKit/PublicHeader.h>
+
+
